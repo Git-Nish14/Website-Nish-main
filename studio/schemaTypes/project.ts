8a88bc6a0159829f0ac079d3d35257678c9a0f69
bhargav <bhargavpatel.info@gmail.com>
2025-02-12 16:39:34 +0530

    projects

---
+++ studio/schemaTypes/project.ts
@@ -0,0 +1,54 @@
+export default {
+    name: "project",
+    title: "Project",
+    type: "document",
+    fields: [
+      {
+        name: "title",
+        title: "Title",
+        type: "string",
+        validation: (Rule:any) => Rule.required(),
+      },
+      {
+        name: "description",
+        title: "Description",
+        type: "text",
+        validation: (Rule:any) => Rule.required(),
+      },
+      {
+        name: "slug",
+        title: "Slug",
+        type: "slug",
+        options: {
+          source: "title",
+          maxLength: 96,
+        },
+        validation: (Rule:any) => Rule.required(),
+      },
+      {
+        name: "image",
+        title: "Image",
+        type: "image",
+        options: {
+          hotspot: true,
+        },
+        fields: [
+          {
+            name: "alt",
+            title: "Alt Text",
+            type: "string",
+          },
+        ],
+        validation: (Rule:any) => Rule.required(),
+      },
+      {
+        name: "link",
+        title: "Project Link",
+        type: "url",
+        validation: (Rule:any) => Rule.required().uri({
+          scheme: ["http", "https"],
+        }),
+      },
+    ],
+  };
+  
